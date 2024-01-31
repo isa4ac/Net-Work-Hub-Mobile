34a5d5bf745acee5e5044fb35e5e57494ff3a170
Isaac Van Meter <ivanmeter28@gmail.com>
2024-01-30 20:51:48 -0500

    made animation a bit smoother, added app icons

--- Net Work Hub/Assets/CustomViews/Rows/BidderRowView.swift
+++ Net Work Hub/Assets/CustomViews/Rows/BidderRowView.swift
@@ -29,22 +29,22 @@ struct BidderRowView: View {
                         //                        .font(isExpanded ? .title2 : .body)
                         Text(name.capitalized)
                             .font(isExpanded ? .title2 : .body)
-                            .animation(.linear(duration: 0.2), value: isExpanded)
+                            .animation(.linear(duration: 0.3), value: isExpanded)
                         // user.yearsOfExperience
                         Text("^[\(yearsOfExperience) Years of Experience](inflect: true)")
                             .font(isExpanded ? .body : .footnote)
                             .foregroundStyle(.secondary)
-                            .animation(.linear(duration: 0.2), value: isExpanded)
+                            .animation(.linear(duration: 0.3), value: isExpanded)
                         Text("^[\(jobsComplete) Jobs Complete](inflect: true)")
                             .font(isExpanded ? .body : .footnote)
                             .foregroundStyle(.secondary)
-                            .animation(.linear(duration: 0.2), value: isExpanded)
+                            .animation(.linear(duration: 0.3), value: isExpanded)
                     }
                     Group {
                         Spacer()
                         Text(quotePrice.formatted(.currency(code: "USD")))
                             .font(isExpanded ? .body : .footnote)
-                            .animation(.smooth(duration: 0.2), value: isExpanded)
+                            .animation(.smooth(duration: 0.3), value: isExpanded)
                         Image(systemName: isExpanded ? "chevron.down" : "chevron.right")
                             .symbolRenderingMode(.hierarchical)
                             .contentTransition(.symbolEffect(.automatic))
@@ -78,7 +78,6 @@ struct BidderRowView: View {
                         }
                     }
                 }
-                .animation(.spring(duration: 0.5), value: isExpanded)
             }
         }
         .foregroundStyle(.primary)
